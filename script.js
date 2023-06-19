11cbe7cbb73177f1580dc1d02bc1c8b060fea629
João Carlos Duarte Vancea <126781923+JoaoVancea@users.noreply.github.com>
2023-06-19 10:28:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35, 
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20, 
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7, 
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21, 
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28, 
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+const estatistica = document.querySelectorAll("[data-estatistica]");
 function somarFuncao(nome, somar) {
     somar.addEventListener("click", (event) => {
         nome.value = parseInt(nome.value) + 1;
@@ -19,3 +52,7 @@ operacao(document.querySelector("#blindagem"), document.querySelector("#somarBli
 operacao(document.querySelector("#nucleos"), document.querySelector("#somarNucleos"), document.querySelector("#subtrairNucleos"));
 operacao(document.querySelector("#pernas"), document.querySelector("#somarPernas"), document.querySelector("#subtrairPernas"));
 operacao(document.querySelector("#foguetes"), document.querySelector("#somarFoguetes"), document.querySelector("#subtrairFoguetes"));
+
+function atualizaEstatistica(peca) {
+    console.log("[peca]");
+}
